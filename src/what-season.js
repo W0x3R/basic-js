0ae5c047f93eb0682bbcafabb4148c8965fafbf5
Stanislau Vyrski <vyr4376@yandex.ru>
2023-04-12 20:50:19 +0300

    finished what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,34 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  if (Object.getOwnPropertyNames(date).length > 0 || !date.getTime)
+    throw new Error("Invalid date!");
+  let getMonth = date.getMonth();
+  if (getMonth === 2 || getMonth === 3 || getMonth === 4) {
+    return "spring";
+  } else if (getMonth === 5 || getMonth === 6 || getMonth === 7) {
+    return "summer";
+  } else if (getMonth === 8 || getMonth === 9 || getMonth === 10) {
+    return "autumn";
+  } else if (getMonth === 11 || getMonth === 0 || getMonth === 1) {
+    return "winter";
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
