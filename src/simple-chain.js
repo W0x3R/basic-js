b30d8baf396ab97ffaef38227fa326f7ee9eb7e7
Stanislau Vyrski <vyr4376@yandex.ru>
2023-04-13 19:41:25 +0300

    finished simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,21 +4,48 @@ const { NotImplementedError } = require("../extensions/index.js");
  * Implement chainMaker object according to task description
  *
  */
+// const chainMaker = {
+//   arr: [],
+//   getLength() {
+//     return this.arr.length;
+//   },
+//   addLink(v) {
+//     this.arr.push(`( ${v} )`);
+//     return this;
+//   },
+//   removeLink(p) {
+//     if (!this.arr[p - 1]) {
+//       this.arr = [];
+//       throw new Error("You can't remove incorrect link!");
+//     }
+//     this.arr.splice(p - 1, 1);
+//     return this;
+//   },
+//   reverseChain() {
+//     this.arr.reverse();
+//     return this;
+//   },
+//   finishChain() {
+//     const res = this.arr.join("~~");
+//     this.arr = [];
+//     return res;
+//   },
+// };
 const chainMaker = {
   arr: [],
   getLength() {
     return this.arr.length;
   },
-  addLink(value) {
-    this.arr.push(`( ${value} )`);
+  addLink(v) {
+    this.arr.push(`( ${v} )`);
     return this;
   },
-  removeLink(place) {
-    if (!this.arr[place - 1]) {
+  removeLink(p) {
+    if (!this.arr[p - 1]) {
       this.arr = [];
       throw new Error("You can't remove incorrect link!");
     }
-    this.arr.splice(place - 1, 1);
+    this.arr.splice(p - 1, 1);
     return this;
   },
   reverseChain() {
